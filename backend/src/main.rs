df473a9653b82a6799038ccd2f082c8a2434ace8
Behzad Khalifeh <bkhalifeh@protonmail.com>
2024-10-03 01:19:12 +0330

    feat: configure redis and swagger

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -2,6 +2,7 @@ use std::env;
 use std::error::Error;
 use std::sync::Arc;
 
+use bb8_redis::RedisConnectionManager;
 use modules::hash::HashService;
 use tokio::net::TcpListener;
 
@@ -15,9 +16,12 @@ use bb8::Pool;
 use diesel_async::pooled_connection::AsyncDieselConnectionManager;
 
 use util::AppState;
+use utoipa::OpenApi;
+use utoipa_swagger_ui::SwaggerUi;
 
 pub mod modules;
 pub mod schema;
+pub mod swagger;
 pub mod util;
 
 #[tokio::main]
@@ -26,6 +30,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let host: String = env::var("APP_HOST")?;
     let port: u16 = env::var("APP_PORT")?.parse()?;
     let database_url: String = env::var("DATABASE_URL")?;
+    let redis_url: String = env::var("REDIS_URL")?;
     let argon_secret: Vec<u8> = BASE64_STANDARD.decode(env::var("ARGON_SECRET")?)?;
     let argon_secret: &mut [u8] = Box::leak(argon_secret.into_boxed_slice());
 
@@ -34,13 +39,19 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let db_pool: Pool<AsyncDieselConnectionManager<diesel_async::AsyncPgConnection>> =
         bb8::Pool::builder().build(db_connection_manager).await?;
 
+    let redis_connection_manager: RedisConnectionManager = RedisConnectionManager::new(redis_url)?;
+    let redis_pool: Pool<RedisConnectionManager> =
+        bb8::Pool::builder().build(redis_connection_manager).await?;
+
     let app_state: AppState<'_> = AppState {
         db_pool,
+        redis_pool,
         hash_service: HashService::new(argon_secret),
     };
 
     let app: Router = Router::new()
-        .merge(modules::get_all_routes())
+        .merge(SwaggerUi::new("/doc").url("/api-docs/openapi.json", swagger::ApiDoc::openapi()))
+        .nest("/api", modules::get_all_routes())
         .with_state(Arc::new(app_state));
     let listener: TcpListener = tokio::net::TcpListener::bind(format!("{host}:{port}")).await?;
     println!("listening on {}", listener.local_addr()?);
--- backend/src/util.rs
+++ backend/src/util.rs
@@ -1,7 +1,8 @@
 use std::sync::Arc;
 
-use axum::{extract::State, Router};
+use axum::{extract::State, http::StatusCode, Router};
 use bb8::{Pool, PooledConnection};
+use bb8_redis::RedisConnectionManager;
 use diesel_async::{pooled_connection::AsyncDieselConnectionManager, AsyncPgConnection};
 
 use crate::modules::hash::HashService;
@@ -9,9 +10,26 @@ use crate::modules::hash::HashService;
 #[derive(Clone)]
 pub struct AppState<'a> {
     pub db_pool: Pool<AsyncDieselConnectionManager<diesel_async::AsyncPgConnection>>,
+    pub redis_pool: Pool<RedisConnectionManager>,
     pub hash_service: HashService<'a>,
 }
 
 pub type TAppState<'a> = State<Arc<AppState<'a>>>;
 pub type TAppRouter<'a> = Router<Arc<AppState<'a>>>;
 pub type TConnection<'a> = PooledConnection<'a, AsyncDieselConnectionManager<AsyncPgConnection>>;
+
+pub fn internal_server_error<E>(err: E) -> (StatusCode, String)
+where
+    E: std::error::Error,
+{
+    (StatusCode::INTERNAL_SERVER_ERROR, err.to_string())
+}
+
+pub fn not_acceptable<E>(err: E) -> (StatusCode, String)
+where
+    E: std::error::Error,
+{
+    (StatusCode::NOT_ACCEPTABLE, err.to_string())
+}
+
+pub type TResponse<T, E> = Result<(StatusCode, T), (StatusCode, E)>;
