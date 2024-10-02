34f952e77311a34063372e3da26c21436084c879
Behzad Khalifeh <bkhalifeh@protonmail.com>
2024-10-03 01:38:54 +0330

    feat: set title and description

--- backend/src/swagger.rs
+++ backend/src/swagger.rs
@@ -8,5 +8,9 @@ use utoipa::OpenApi;
     ),
     paths(auth::sign_up_post,),
     components(schemas(auth::UserSignUpDto, auth::User)),
+    info(
+        title = "Wener",
+        description = "The Wener API description",
+    ),
 )]
 pub struct ApiDoc;
